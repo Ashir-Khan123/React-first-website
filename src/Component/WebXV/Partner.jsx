7b33a0588cfd82966210985b5dadcddf610ecb83
Ashir-Khan123 <ashir3862@gmail.com>
2025-04-26 20:35:19 +0500

    set nested list

--- src/Component/WebXV/Partner.jsx
+++ src/Component/WebXV/Partner.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+import picOne from '../../assets/partner-1.png';
+import picTwo from '../../assets/partner-2.png';
+import picThree from '../../assets/partner-3.png';
+import picFour from '../../assets/partner-4.png';
+import picFive from '../../assets/partner-5.png';
+import picSix from '../../assets/partner-6.png';
+import picSeven from '../../assets/partner-7.png';
 
 const Partner = () => {
   return (
-    <div></div>
+    <div className='bg-black'>
+      <div className='py-5'>
+        <div className='uppercase text-white'>
+          <h3>our partners</h3>
+        </div>
+        <div className='flex items-center py-5'>
+          <div><img src={picOne} alt="picOne" className='w-[40%]'/></div>
+          <div><img src={picTwo} alt="" className='w-[100%]'/></div>
+          <div><img src={picThree} alt="" className='w-[50%]'/></div>
+          <div><img src={picFour} alt="" className='w-[70%]'/></div>
+          <div><img src={picFive} alt="" className='w-[70%]'/></div>
+          <div><img src={picSix} alt="" className='w-[70%]'/></div>
+          <div><img src={picSeven} alt="" className='w-[70%]'/></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
