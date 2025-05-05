4ad066b531feb440bc6fd7ff632913c71f476e6a
Ashir-Khan123 <ashir3862@gmail.com>
2025-05-05 18:10:27 +0500

    about-sec-pic

--- src/Component/WebXV/AboutUs.jsx
+++ src/Component/WebXV/AboutUs.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import aboutImage from 
 
 const AboutUs = () => {
   return (
-    <div></div>
+    <div className='grid grid-cols-12'>
+      <div className='lg:col-span-6 col-span-12'>
+        <div>
+          <p>About us</p>
+          <h2>Who we are</h2>
+          <p>XV is a community-focused project led by a highly experienced team of Web3 builders and traders. Our founder A On made his name in the industry moderating for several early crypto projects before the 2021 bull run.</p>
+          <p>XV is also supported by several team members that are actively involved in the day-to-day running of the community. Join us on Telegram and Twitter.</p>
+        </div>
+      </div>
+      <div className='lg:col-span-6 col-span-12'>
+        <img src="" alt="" />
+      </div>
+    </div>
   )
 }
 
--- src/Component/WebXV/Partner.jsx
+++ src/Component/WebXV/Partner.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import picOne from '../../assets/partner-1.png';
+import picTwo from '../../assets/partner-2.png';
+import picThree from '../../assets/partner-3.png';
+import picFour from '../../assets/partner-4.png';
+import picFive from '../../assets/partner-5.png';
+import picSix from '../../assets/partner-6.png';
+import picSeven from '../../assets/partner-7.png';
 
 const Partner = () => {
   return (
-    <div></div>
+    <div className='bg-[#0B0E12] text-[#335DD2] text-xl px-15 font-bold w-full'>
+      <div className='flex justify-center py-8'>
+        <h3 className='uppercase'>our partners</h3>
+      </div>
+      <div className='flex gap-8 pb-5 w-full max-w-[100px]'>
+        <img src={picOne} alt="" />
+        <img src={picTwo} alt="" />
+        <img src={picThree} alt="" />
+        <img src={picFour} alt="" />
+        <img src={picFive} alt="" />
+        <img src={picSix} alt="" />
+        <img src={picSeven} alt="" />
+      </div>
+    </div>
   )
 }
 
