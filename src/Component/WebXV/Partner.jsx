26279f77dba380b700fefc0f98672d1190d7701b
Ashir-Khan123 <ashir3862@gmail.com>
2025-05-05 16:27:54 +0500

    partner-section

--- src/Component/WebXV/Partner.jsx
+++ src/Component/WebXV/Partner.jsx
@@ -1,11 +1,4 @@
 import React from 'react';
-import picOne from '../../assets/partner-1.png';
-import picTwo from '../../assets/partner-2.png';
-import picThree from '../../assets/partner-3.png';
-import picFour from '../../assets/partner-4.png';
-import picFive from '../../assets/partner-5.png';
-import picSix from '../../assets/partner-6.png';
-import picSeven from '../../assets/partner-7.png';
 
 const Partner = () => {
   return (
