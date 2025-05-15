23a33e56f794b33d4a710e5f2ddf09f3ea4f0b6f
Ashir-Khan123 <ashir3862@gmail.com>
2025-05-15 17:43:43 +0500

    navbar set

--- src/Component/Subcompnent/Navbar.jsx
+++ src/Component/Subcompnent/Navbar.jsx
@@ -5,30 +5,79 @@ import {
   NavbarCollapse,
   NavbarToggle,
 } from "flowbite-react";
-import { NavLink } from "react-router";
-import logo from "../../assets/logo.png"
+import { NavLink } from "react-router"; // ✔️ make sure you're using `react-router-dom`
+import logo from "../../assets/logo.png";
+import { useState } from "react";
 
 export default function NavbarComp() {
+  const [Open, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!Open);
+  };
+
+  const handleLinkClick = () => {
+    setIsOpen(false); 
+  };
+
   return (
-    <Navbar className="flex justify-around fixed w-full z-50 top-0">
-      <NavbarBrand>
-        <span className="w-[110px]">
-          <img src={logo} alt={"logo"}/>
-        </span>
-      </NavbarBrand>
-      <div className="flex md:order-2">
-        <Button className="uppercase hidden md:block">Register</Button>
-        <NavbarToggle className="cursor-pointer"/>
-      </div>
-      <NavbarCollapse>
-        <NavLink className={({ isActive }) => isActive ? "text-white" : "text-gray-400" } to="/" > Wev XV </NavLink>
-        <NavLink className={({ isActive }) => isActive ? "text-white" : "text-gray-400" } to="/All_project">All Projects</NavLink>
-        <NavLink className={({ isActive }) => isActive ? "text-white" : "text-gray-400" } to="/Community">Community</NavLink>
-        <NavLink className={({ isActive }) => isActive ? "text-white" : "text-gray-400" } to="/About_us">About Us</NavLink>
-      </NavbarCollapse>
-    </Navbar>
+    <div>
+      <Navbar className="flex justify-around fixed w-full z-50 top-0">
+        <NavbarBrand>
+          <span className="w-[110px]">
+            <img src={logo} alt={"logo"} />
+          </span>
+        </NavbarBrand>
+        <div className="flex md:order-2">
+          <Button className="uppercase hidden md:block">Register</Button>
+          <NavbarToggle onClick={handleToggle} className="cursor-pointer" />
+        </div>
+        <NavbarCollapse
+          className={`font-semibold leading-10 text-center ${
+            Open ? "block" : "hidden"
+          } md:block`}>
+          <NavLink
+            onClick={handleLinkClick}
+            className={({ isActive }) =>
+              isActive ? "text-white" : "text-gray-400"
+            }
+            to="/"
+          >
+            Wev XV
+          </NavLink>
+          <NavLink
+            onClick={handleLinkClick}
+            className={({ isActive }) =>
+              isActive ? "text-white" : "text-gray-400"
+            }
+            to="/All_project"
+          >
+            All Projects
+          </NavLink>
+          <NavLink
+            onClick={handleLinkClick}
+            className={({ isActive }) =>
+              isActive ? "text-white" : "text-gray-400"
+            }
+            to="/Community"
+          >
+            Community
+          </NavLink>
+          <NavLink
+            onClick={handleLinkClick}
+            className={({ isActive }) =>
+              isActive ? "text-white" : "text-gray-400"
+            }
+            to="/About_us"
+          >
+            About Us
+          </NavLink>
+        </NavbarCollapse>
+      </Navbar>
+    </div>
   );
 }
+
 
 
 
